3efd4998103db9f1fe43b003432ca745a26c8a95
shaochuwang <shaochuwang@tencent.com>
2024-01-28 16:16:35 +0800

    feat: 更新细节

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+// adjust root id according to your component
+ReactDOM.createRoot(document.getElementById('rc_drag_upload_comp')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
